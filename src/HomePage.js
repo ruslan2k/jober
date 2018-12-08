506b6b2dc801d37c5ed48c5adb4c2fd984e24446
Ruslan Karachun <free.changer@gmail.com>
2018-12-08 23:00:13 +0300

    Added Contacts Page

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,9 +4,7 @@ import {
   TabBarItem,
   Article
 } from "react-weui";
-
-import "weui";
-import "react-weui/build/packages/react-weui.css";
+import ContactsPage from './ContactsPage';
 
 // import IconButton from '../home/images/icon_nav_button.png';
 // import IconMsg from '../home/images/icon_nav_msg.png';
@@ -19,19 +17,7 @@ export default class HomePage extends Component {
         return (
             <Tab type="tabbar">
                 <TabBarItem icon={<img src={IconGreenButton}/>} label="Tab1">
-                    <Article>
-                        <h1>Page 1</h1>
-                        <section>
-                            <h2 className="title">Heading</h2>
-                            <section>
-                                <h3>1.1 Title</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat. Duis aute</p>
-                            </section>
-                        </section>
-                    </Article>
+                  {ContactsPage}
                 </TabBarItem>
                 <TabBarItem icon={<img src={IconGreenButton}/>} label="Tab2">
                     <Article>
