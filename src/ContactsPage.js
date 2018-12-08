3876b58248e19ff8a9147c26d342ca440675791c
Ruslan Karachun <free.changer@gmail.com>
2018-12-08 22:55:17 +0300

    Added Contacts Page

---
+++ src/ContactsPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Article } from 'react-weui';
+
+const ContactsPage = (props) => {
+  return (
+    <Article>
+        <h1>Contacts Page</h1>
+        <section>
+            <h2 className='title'>Heading</h2>
+            <section>
+                <h3>1.1 Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute</p>
+            </section>
+        </section>
+    </Article>
+  );
+}
+
+export default ContactsPage;
+
