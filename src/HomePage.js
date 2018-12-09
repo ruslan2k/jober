aa4779d47d4ba3b629c9394f63c6379464e01507
Ruslan Karachun <free.changer@gmail.com>
2018-12-09 22:24:13 +0300

    Added contacts

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,40 +1,41 @@
 import React, { Component } from 'react';
-import {
-  Tab,
-  TabBarItem,
-  Article
-} from "react-weui";
+import { Tab, NavBarItem, Article } from 'react-weui';
 import ContactsPage from './ContactsPage';
 
-// import IconButton from '../home/images/icon_nav_button.png';
-// import IconMsg from '../home/images/icon_nav_msg.png';
-// import IconArticle from '../home/images/icon_nav_article.png';
+const contacts = [
+  {
+    name: 'Vasya P',
+  },
+  {
+    name: 'Petya V',
+  },
+];
 
-const IconGreenButton = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Green_dot.svg/450px-Green_dot.svg.png';
+const Item = (props) => <li>{'Item'}</li>;
 
 export default class HomePage extends Component {
     render() {
+        const contactsList = contacts.map(c => <li>{c.name}</li>);
         return (
-            <Tab type="tabbar">
-                <TabBarItem icon={<img src={IconGreenButton}/>} label="Tab1">
-                  {ContactsPage}
-                </TabBarItem>
-                <TabBarItem icon={<img src={IconGreenButton}/>} label="Tab2">
+            <Tab type="navbar">
+                <NavBarItem label="Nav1">
+                  <ContactsPage />
+                </NavBarItem>
+                <NavBarItem label="Nav2">
                     <Article>
                         <h1>Page 2</h1>
                         <section>
                             <h2 className="title">Heading</h2>
                             <section>
                                 <h3>2.1 Title</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat. Duis aute</p>
+                              <ul>
+                                {contactsList}
+                              </ul>
                             </section>
                         </section>
                     </Article>
-                </TabBarItem>
-                <TabBarItem icon={<img src={IconGreenButton}/>} label="Tab3">
+                </NavBarItem>
+                <NavBarItem label="Nav3">
                     <Article>
                         <h1>Page 3</h1>
                         <section>
@@ -48,7 +49,7 @@ export default class HomePage extends Component {
                             </section>
                         </section>
                     </Article>
-                </TabBarItem>
+                </NavBarItem>
             </Tab>
         );
     }
