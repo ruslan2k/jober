0f3265054f4c0aa5c65df7d4a727a89b1769fe02
Ruslan Karachun <free.changer@gmail.com>
2018-12-10 23:41:39 +0300

    Added random users

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Tab, NavBarItem, Article } from 'react-weui';
 import ContactsPage from './ContactsPage';
-
-const contacts = [
-  {
-    name: 'Vasya P',
-  },
-  {
-    name: 'Petya V',
-  },
-];
 
 const Item = (props) => <li>{'Item'}</li>;
 
 export default class HomePage extends Component {
-    render() {
-        const contactsList = contacts.map(c => <li>{c.name}</li>);
+  constructor(props) {
+    super(props);
+    this.state = {contacts: []};
+  }
+  componentDidMount() {
+    axios('https://randomuser.me/api/?results=10')
+      .then(({ data }) => this.setState({contacts: data.results}));
+  }
+  render() {
+        const contactsList = this.state.contacts.map(c => <li>{c.name.first}</li>);
         return (
             <Tab type="navbar">
                 <NavBarItem label="Nav1">
-                  <ContactsPage />
+                    <Article>
+                        <h1>Page 2</h1>
+                        <section>
+                            <h2 className="title">Heading</h2>
+                            <section>
+                                <h3>2.1 Title</h3>
+                              <ul>
+                                {contactsList}
+                              </ul>
+                            </section>
+                        </section>
+                    </Article>
                 </NavBarItem>
                 <NavBarItem label="Nav2">
                     <Article>
